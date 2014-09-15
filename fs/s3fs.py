ca0ea11010def81a8fa1964e4a2b5f62f4f89d3e
Piotr Mitros <pmitros@mitx.mit.edu>
2014-09-15 10:54:20 -0400

    Removed S3 AWS permission exceptions, which are in boto, and which break VPC auth

--- fs/s3fs.py
+++ fs/s3fs.py
@@ -108,12 +108,6 @@ class S3FS(FS):
             prefix = prefix + separator
         if isinstance(prefix,unicode):
             prefix = prefix.encode("utf8")
-        if aws_access_key is None:
-            if "AWS_ACCESS_KEY_ID" not in os.environ:
-                raise CreateFailedError("AWS_ACCESS_KEY_ID not set")
-        if aws_secret_key is None:
-            if "AWS_SECRET_ACCESS_KEY" not in os.environ:
-                raise CreateFailedError("AWS_SECRET_ACCESS_KEY not set")
         self._prefix = prefix
         self._tlocal = thread_local()
         super(S3FS, self).__init__(thread_synchronize=thread_synchronize)
